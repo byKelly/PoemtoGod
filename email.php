329ea9738ed78b082240688323e7b96b9203c7d3
byKelly <kelly@nkbrown.us>
2016-08-22 19:55:59 -0700

    Changed to original copied code

--- email.php
+++ email.php
@@ -1,5 +1,6 @@
 
 <?php
-    mail('byempresskelly@yahoo.com', $_POST['name'], $_POST['_replyto']); $_Post['message']);
+    mail('byempresskelly@yahoo.com', $_POST['subject'], $_POST['message']);
 ?>
 <p>Your email has been sent.</p>
+
