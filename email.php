eb66be83a198d82c52a2df0a776a36595784af28
byKelly <kelly@nkbrown.us>
2016-08-22 19:42:26 -0700

    Updated email address use

--- email.php
+++ email.php
@@ -1,5 +1,5 @@
 
 <?php
-    mail('youremail@example.com', $_POST['subject'], $_POST['message']);
+    mail('byempresskelly@yahoo.com', $_POST['subject'], $_POST['message']);
 ?>
 <p>Your email has been sent.</p>
