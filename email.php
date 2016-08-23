d5ff16038dff15ead97042613ad1ef05f9837911
byKelly <kelly@nkbrown.us>
2016-08-22 19:50:05 -0700

    Corrected wording of boxes for comment box
    Added "Your name" "Your email" and "Your message"

--- email.php
+++ email.php
@@ -1,5 +1,5 @@
 
 <?php
-    mail('byempresskelly@yahoo.com', $_POST['subject'], $_POST['message']);
+    mail('byempresskelly@yahoo.com', $_POST['Your name'], $_POST['Your email']); $_Post['Your message']);
 ?>
 <p>Your email has been sent.</p>
